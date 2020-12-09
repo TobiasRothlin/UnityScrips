957c2409f2e1a1da44ac50218c392e9cb783c1f6
TobiasRothlin <tobias@rothlin.com>
2020-12-09 16:20:34 +0100

    Bugs Fixed

--- Cam/LinkPanTiltToObject.cs
+++ Cam/LinkPanTiltToObject.cs
@@ -30,9 +30,11 @@ public class LinkPanTiltToObject : MonoBehaviour
         Vector3 YangleVector = new Vector3(CameraToTarget.x, 0, CameraToTarget.z);        
 
         YAngle = (float)(Math.Acos(CameraToTarget.x / (YangleVector.magnitude)) * (360 / (2 * Math.PI)));
-        YAngle *= SignumFunction(CameraToTarget.z);
 
-        XAngle = (float)(Math.Acos(CameraToTarget.y / (XangleVector.magnitude)) * (360 / (2 * Math.PI)));
+        YAngle *= SignumFunction(CameraToTarget.z);
+        Debug.Log("Z Value: " + CameraToTarget.z.ToString() + " | Signum Function: " + SignumFunction(CameraToTarget.z).ToString() + " | YAngle: " + YAngle.ToString());
+
+        XAngle = (float)(Math.Acos(CameraToTarget.y / (CameraToTarget.magnitude)) * (360 / (2 * Math.PI)));
         
 
         transform.rotation = Quaternion.Euler(XAngle - 90, YAngle + 90, 0);
@@ -43,9 +45,14 @@ public class LinkPanTiltToObject : MonoBehaviour
 
     float SignumFunction(float value)
     {
-        if(value <= 0)
+        if(value < 0)
         {
             return 1F;
+        }
+
+        if(value.Equals(0))
+        {
+            return 0;
         }
 
         return - 1F;
